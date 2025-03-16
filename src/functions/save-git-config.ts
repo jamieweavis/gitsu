37c6b960dddc41c180c6f99ec0a3c6e9bfd600fe
Jamo <jamie.weavis@gmail.com>
2025-03-16 17:37:17 +0000

    fix: save local and global git configs

--- src/functions/save-git-config.ts
+++ src/functions/save-git-config.ts
@@ -1,14 +1,18 @@
-import child_process from 'node:child_process';
+import { exec } from 'node:child_process';
 import logSymbols from 'log-symbols';
 
 import type { IUser } from '../types';
 
 export default async (user: IUser): Promise<boolean> => {
   try {
-    await child_process.exec(
+    // If user is in a git repo, save user to local git config
+    await exec(
+      `cd ${process.cwd()} && git config user.name "${user.name}" && git config user.email "${user.email}"`,
+    );
+    // Save user to global git config
+    await exec(
       `git config --global user.name "${user.name}" && git config --global user.email "${user.email}"`,
     );
-
     return true;
   } catch (error) {
     console.error(`${logSymbols.error} Failed to save git config`, error);
