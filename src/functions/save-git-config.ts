cfcdb630fbd680759278f47a98f4481766d6ff0e
Jamie <jamie.weavis@gmail.com>
2025-04-05 16:29:46 +0100

    fix: handle git exec asynchronously

--- src/functions/save-git-config.ts
+++ src/functions/save-git-config.ts
@@ -1,16 +1,14 @@
 import { exec } from 'node:child_process';
+import { promisify } from 'node:util';
 import logSymbols from 'log-symbols';
+
+const execPromise = promisify(exec);
 
 import type { IUser } from '../types';
 
 export default async (user: IUser): Promise<boolean> => {
   try {
-    // If user is in a git repo, save user to local git config
-    await exec(
-      `cd ${process.cwd()} && git config user.name "${user.name}" && git config user.email "${user.email}"`,
-    );
-    // Save user to global git config
-    await exec(
+    await execPromise(
       `git config --global user.name "${user.name}" && git config --global user.email "${user.email}"`,
     );
     return true;
